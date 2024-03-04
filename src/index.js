3a792a47edba2d121053000cb6d6bfd706337ac0
Faizan <sfaizan78692@gmail.com>
2024-03-04 09:08:05 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import dotenv from 'dotenv'
 import connetToDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({ path: './env'})
 
-connetToDB();
+connetToDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log('Error while talking with database: ', error)
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is listening on port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log('Mongo DB connection failed')
+})
 
 
 // ;(async () => {
