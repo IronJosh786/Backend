9283bbb9fab2dc1ea19e9b72bf016fe4218f935f
Faizan <sfaizan78692@gmail.com>
2024-03-08 21:21:21 +0530

    add all blank controller and route files

--- src/app.js
+++ src/app.js
@@ -1,27 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 // To get data from request
 
-app.use(express.json({limit: '16kb'})) // from json
-app.use(express.urlencoded({extended: true, limit: '16kb'})) // from url
-app.use(express.static("public")) // static files like pdf, image to store in temp
-app.use(cookieParser()) // for secure CRUD operations from server to browser
+app.use(express.json({ limit: "16kb" })); // from json
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // from url
+app.use(express.static("public")); // static files like pdf, image to store in temp
+app.use(cookieParser()); // for secure CRUD operations from server to browser
 
 // importing routes
 
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
+import tweetRouter from "./routes/tweet.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import videoRouter from "./routes/video.route.js";
+import commentRouter from "./routes/comment.route.js";
+import likeRouter from "./routes/like.route.js";
+import playlistRouter from "./routes/playlist.route.js";
+import dashboardRouter from "./routes/dashboard.route.js";
 
 // declaring routes
 
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
-export { app }
+export { app };
